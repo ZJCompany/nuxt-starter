49414bfb6171141ef4eeaee095d0600a508dcc91
Zeyad.Khadeeda <25@@mklj65>
2021-11-16 15:10:29 +0300

    add atoms

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,25 @@
   <div>
     <BaseFooButton title="submit" class="bg-green-500 text-white"  @add-itam="add" type="button" ></BaseFooButton>
     <br/>
-      <BaseFooButton  title="Save" class="bg-red-500 text-white"  @add-itam="add" type="button" ></BaseFooButton>
+    {{text}}
 
-    <NuxtLink to="/about">
+    <div class="p-3 bg-red-500 border-0">
+        <AtomsLabel class="text-lg" value="Text input"></AtomsLabel>
+       <AtomsInput class="m-3 " v-model:modelValue="text"  ></AtomsInput>
+    </div>
+     
+
+    <!-- <NuxtLink to="/about">
       About (internal link that belongs to the Nuxt App)
     </NuxtLink>
-    <a href="https://nuxtjs.org">External Link to another page</a>
+    <a  href="https://nuxtjs.org">External Link to another page</a> -->
   </div>
 </template>
 
 <script setup>
+import {ref} from 'vue'
            function add() {
              console.log('work');
            }
+           let text=ref('')
 </script>
