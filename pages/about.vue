a1488f1ba4734dd96529e667d74922c463f0102c
Zeyad.Khadeeda <25@@mklj65>
2021-11-16 14:17:39 +0300

    add tailwind css

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,3 @@
 <template>
-Hello
+about Page
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <BaseFooButton  @add-itam="add" type="button" ><p>dsds</p></BaseFooButton>
     <h1>Home page</h1>
     <NuxtLink to="/about">
       About (internal link that belongs to the Nuxt App)
@@ -7,3 +8,9 @@
     <a href="https://nuxtjs.org">External Link to another page</a>
   </div>
 </template>
+
+<script setup>
+           function add() {
+             console.log('work');
+           }
+</script>
