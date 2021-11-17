9d9a388df7f9131b85bb9b8355a4c88e4d603380
Zeyad.Khadeeda <25@@mklj65>
2021-11-17 11:26:45 +0300

    remove bulid file

--- .output/public/_nuxt/AuthenticationCardLogo-caa4b00c.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as n,d as t}from"./entry-c066cf72.mjs";const e=t({components:{}});function o(r,c,s,a,p,_){return null}var d=n(e,[["render",o]]);export{d as default};
--- .output/public/_nuxt/DropdownLink-2e338566.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as n,d as e}from"./entry-c066cf72.mjs";const o=e({components:{},props:["href","as"]});function r(s,t,p,c,a,f){return null}var d=n(o,[["render",r]]);export{d as default};
